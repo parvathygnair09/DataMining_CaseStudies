e7dbee43f1a5f56ff6a4fa530cfd8bba849c6e42
Parvathy Gopinathan Nair <parugn.99@gmail.com>
2017-10-21 11:33:15 -0700

    Code to pull data

--- StockMarketPredictions.R
+++ StockMarketPredictions.R
@@ -1,11 +1,13 @@
 ## Project : Stock Market Predictions
 ## Explore and learn data import from MySQL and time series data analysis with R
+## this case study is taken from the book "Data Ming With R - Learning with Case Studies" by Luis Torgo
 ## Date Created : 10/20/2017
-## ==============================================================================
+## =====================================================================================================
 
+source("requirements.R")
 library(DBI)
 library(RMySQL)
+
 drv <- dbDriver("MySQL")
-#ch <- dbConnect(drv, dbname = "Projects","root","KunjuPraghu2019")
-ch <- dbConnect(dbDriver("MySQL"), user="root", password="KunjuPraghu2019", dbname="Projects", host="127.0.0.1", port=3306)
+ch <- dbConnect(dbDriver("MySQL"), user = user, password = password, dbname = dbname, host = host, port = port)
 
